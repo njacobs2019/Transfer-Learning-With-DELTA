498554e5db0f879f28d5980048f538e2a3ab2165
Nicholas Jacobs <nicholas.jacobs@maine.edu>
2023-04-30 20:36:51 +0000

    Improved setup

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 Transfer-Learning-With-DELTA
+
+# Run the setup script
+```
+curl https://raw.githubusercontent.com/njacobs2019/Transfer-Learning-With-DELTA/main/setup.sh -o setup.sh && chmod +x setup.sh && ./setup.sh
+```
+
--- setup.sh
+++ setup.sh
@@ -1,8 +1,15 @@
-echo "Installing VS-Code Extensions"
+echo "Updating Ubuntu"
+sudo apt update  && sudo apt upgrade -y
 
+echo "Cloning the repo"
+git clone https://github.com/njacobs2019/Transfer-Learning-With-DELTA.git
+
+echo "Installing VS-Code Extensions"
 code --install-extension ms-python.python
 code --install-extension streetsidesoftware.code-spell-checker
 code --install-extension ms-toolsai.jupyter
+code --install-extension ms-python.black-formatter
+code --install-extension ms-python.isort
 
 pip install --quiet jupyter
 
@@ -17,3 +24,8 @@ git config --global user.email "nicholas.jacobs@maine.edu"
 git config --global user.name "Nicholas Jacobs"
 
 pip install --upgrade torch
+
+echo "Adding additional ssh keys"
+curl https://github.com/njacobs2019.keys -o njacobs2019.keys
+cat njacobs2019.keys >> ~/.ssh/authorized_keys
+rm njacobs2019.keys
