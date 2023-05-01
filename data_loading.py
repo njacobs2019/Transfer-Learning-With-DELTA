0e1cd6496f6726df7e4479d665d059697fbcadce
Nicholas Jacobs <nicholas.jacobs@maine.edu>
2023-05-01 00:45:09 +0000

    changed to dataset function

--- data_loading.py
+++ data_loading.py
@@ -2,17 +2,20 @@ from torch.utils.data import DataLoader, random_split
 from torchvision import datasets, transforms
 
 
-def cifar10_dataloaders(BATCH_SIZE, val=False, WORKERS=2):
+def cifar10_datasets(val=False):
     """
-    Downloads CIFAR-10 dataset and creates DataLoader objects.
+    Downloads CIFAR-10 dataset and returns a dictionary of dataset objects.
+
+    This function applies data transformation pipelines for the training and testing sets,
+    and optionally creates a validation set from the training data.
 
     Args:
-        batch_size (int): The batch size for the DataLoader.
-        val (bool, optional): Whether to create a validation DataLoader. Defaults to False.
-        workers (int, optional): Number of workers for DataLoader. Defaults to 2.
+        val (bool, optional): Whether to create a validation set. Defaults to False.
 
     Returns:
-        dict: A dictionary containing DataLoader objects for train, test, and optionally, validation set.
+        dict: A dictionary containing dataset objects for the train, test, and
+              optionally, validation set. The keys in the dictionary are "train",
+              "test", and "val" (if a validation set is requested).
     """
 
     # Training set data transformation pipeline
@@ -37,7 +40,6 @@ def cifar10_dataloaders(BATCH_SIZE, val=False, WORKERS=2):
             ),  # Normalize to ImageNet for better comparison
         ]
     )
-
     ds = datasets.CIFAR10(
         root="./.data", train=True, download=True, transform=transform_train
     )
@@ -46,27 +48,10 @@ def cifar10_dataloaders(BATCH_SIZE, val=False, WORKERS=2):
         root="./.data", train=False, download=True, transform=transform_test
     )
 
-    test_loader = DataLoader(
-        test_ds, batch_size=BATCH_SIZE, shuffle=True, num_workers=WORKERS
-    )
-
     # Case with validation set
     if val:
         train_ds, val_ds = random_split(ds, [45000, 5000])
-
-        train_loader = DataLoader(
-            train_ds, batch_size=BATCH_SIZE, shuffle=True, num_workers=WORKERS
-        )
-
-        val_loader = DataLoader(
-            val_ds, batch_size=BATCH_SIZE, shuffle=True, num_workers=WORKERS
-        )
-
-        return {"train": train_loader, "val": val_loader, "test": test_loader}
+        return {"train": train_ds, "val": val_ds, "test": test_ds}
 
     # Case without validation set
-    train_loader = DataLoader(
-        train_ds, batch_size=BATCH_SIZE, shuffle=True, num_workers=WORKERS
-    )
-
-    return {"train": train_loader, "test": test_loader}
+    return {"train": ds, "test": test_ds}
